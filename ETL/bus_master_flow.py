a6d4f7feb4ffe8c48e6b3f54bc15e1d3384093fe
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-07-06 11:31:25 +0100

    first commit

---
+++ ETL/bus_master_flow.py
@@ -0,0 +1,15 @@
+from bus_live_locations import get_live_bus_locations
+from compare_bus_times import compare_bus_times
+from write_to_bq import write_late_buses_bq
+from prefect import flow
+
+
+@flow
+def main_flow():
+    live_buses = get_live_bus_locations()
+    compare_bus_times(wait_for=[live_buses])
+    write_late_buses_bq(wait_for=[compare_bus_times])
+
+
+if __name__ == "__main__":
+    main_flow()
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,9 @@
+#GCP settings
+project_id = "bus-project-390317" 
+region = "europe-west6"
+
+#Bucket
+bucket_name ="bus-bucket-ka4"
+storage_class = "STANDARD"
+#Artifact registry
+registry_id="bus-tracking-docker"
